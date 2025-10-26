69fcef9809944e2c0691c1ff06c31e2413a238d3
RenatTest <renattesttask@gmail.com>
2025-10-26 19:49:13 +0200

    feat(LA-layers): refactoring

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# login_app
+# LOGIN APP PROJECT
 
-A new Flutter project.
+This is a simple project for login/register to Firebase with email and password.
 
-## Getting Started
+## PROJECT STRUCTURE
 
-This project is a starting point for a Flutter application.
+## PROJECT TECHNOLOGIES
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- Navigation - go_router
+- State Management - flutter_bloc (cubit)
+- DI - get_it
+- Token Storage - flutter_secure_storage
+- API - Firebase
--- lib/features/home/presentatuin/ui/screens/home_page_screen.dart
+++ lib/features/home/presentatuin/ui/screens/home_page_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:login_app/features/app/page_names.dart';
+import 'package:login_app/features/home/presentatuin/ui/widgets/home_button.dart';
+import 'package:login_app/features/home/presentatuin/ui/widgets/home_text.dart';
 import 'package:login_app/features/login/presentation/cubit/auth_cubit.dart';
 
 class HomePageScreen extends StatelessWidget {
@@ -24,36 +26,14 @@ class HomePageScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           spacing: 10,
           children: [
-            Text(
-              'Welcome ${user?.email ?? ""}',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            ElevatedButton(
+            HomeText(text: 'Welcome ${user?.email ?? ""}'),
+            HomeButton(
+              buttonText: 'Logout',
               onPressed: () async {
                 await context.read<AuthCubit>().signOut();
                 // ignore: use_build_context_synchronously
                 context.goNamed(ScreenNames.loginPage);
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.transparent,
-                shadowColor: Colors.transparent,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                side: BorderSide(width: 2.0, color: Colors.white),
-              ),
-              child: Text(
-                'Logout',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
             ),
           ],
         ),
