b64c6f1a06fcb48bcb6787f2a120eb5ed143b4f6
RenatTest <renattesttask@gmail.com>
2025-10-27 06:37:52 +0200

    feat(LA-redirect): added redirect in go-router

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,9 +1,23 @@
 import 'package:go_router/go_router.dart';
+import 'package:login_app/core/storage/secure_storage/secure_storage.dart';
 import 'package:login_app/features/app/page_names.dart';
 import 'package:login_app/features/login/presentation/ui/screens/login_page_screen.dart';
 import 'package:login_app/features/home/presentatuin/ui/screens/home_page_screen.dart';
 
 final router = GoRouter(
+  redirect: (context, state) async {
+    final token = await SecureStorage.instance.getToken();
+
+    if (token == '' && state.matchedLocation != '/') {
+      return '/';
+    }
+
+    if (token != '' && state.matchedLocation == '/') {
+      return '/home-page';
+    }
+
+    return null;
+  },
   initialLocation: '/',
   routes: [
     GoRoute(
